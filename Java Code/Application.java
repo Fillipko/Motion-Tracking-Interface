353f69770836304ae7b5f1db257f08126fe6b0a2
Fillip <fillip.cannard@gmail.com>
2019-07-16 09:08:08 -0700

    Clean up

--- Java Code/Application.java
+++ Java Code/Application.java
@@ -1,24 +1,22 @@
-import javax.swing.event.*;
-import javax.swing.JButton;
-import javax.swing.JFrame;
 import javax.swing.*;
-import javax.swing.AbstractButton;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
+import java.awt.event.*;
+import java.awt.GridBagLayout;
 
-public class Application implements ActionListener{
-    
+public class Application implements ActionListener{    
     JFrame frame = new JFrame();
     JButton button = new JButton("Setup hand here");
 
     public Application() {
-        frame.setSize(500, 500);
+        //Frame Setup
+        frame.setSize(800, 500);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setVisible(true);
-        button.setSize(250, 50);
+        frame.setLayout(new GridBagLayout());
+        //Button Setup
+        frame.add(button);
         button.setVisible(true);
-        frame.getContentPane().add(button);
         button.addActionListener(this);
+        Container.add();
     }
     public static void main(String[] args) {
         new Application();
