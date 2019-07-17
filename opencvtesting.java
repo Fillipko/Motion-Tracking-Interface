ace152c1c0e931dce09ea65db3223b478ffafb1c
sidharthdaga <50777618+sidharthdaga@users.noreply.github.com>
2019-07-17 13:52:47 -0700

    Add files via upload

--- opencvtesting.java
+++ opencvtesting.java
@@ -1,17 +1,26 @@
 
+import java.awt.EventQueue;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.concurrent.TimeUnit;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+import javax.swing.border.EmptyBorder;
+import java.awt.Graphics;
 
 import org.opencv.core.Core;
 import org.opencv.core.CvType;
 import org.opencv.core.Mat;
+import org.opencv.videoio.VideoCapture;
 
 public class opencvtesting implements ActionListener{
 	JFrame frame = new JFrame();
 	JButton button = new JButton("Setup hand here");
+	private JPanel contentPane;
+	VideoCapture videoCap = new VideoCapture();
 
 	public opencvtesting() {
 		frame.setSize(500, 500);
@@ -21,18 +30,31 @@ public class opencvtesting implements ActionListener{
 		button.setVisible(true);
 		frame.getContentPane().add(button);
 		button.addActionListener(this);
+
+		contentPane.setBounds(0, 0, 1280, 720);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(0, 0, 0, 0));
+		frame.setContentPane(contentPane);
+		contentPane.setLayout(null);
 	}
 
 	public static void main(String[] args) {
-		//		System.out.println("Welcome to OpenCV " + Core.VERSION);
-		//		System.out.println(System.getProperty("java.library.path"));
-		//		System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
-		//		Mat m  = Mat.eye(3, 3, CvType.CV_8UC1);
-		//		System.out.println("m = " + m.dump());
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					JFrame frame = new JFrame();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
 		new opencvtesting();
 	}
+	
 	public void actionPerformed(ActionEvent event) {
 		button.setVisible(false);
+
 	}
 }
 
