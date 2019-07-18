4ec9d666b0bc0e7b681bfc36e3ce94f9b508ffd0
David Zager <walkdownlane22@gmail.com>
2019-07-18 10:53:24 -0700

    Windows displays Webcam footage and can be toggled

--- Java Code/DisplayVideo.java
+++ Java Code/DisplayVideo.java
@@ -1,71 +1,89 @@
-import org.opencv.core.*;
-import org.opencv.videoio.VideoCapture;
-
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 
+import javax.imageio.ImageIO;
 import javax.swing.*;
+import javax.swing.border.EmptyBorder;
 
-public class DisplayVideo implements ActionListener
+public class DisplayVideo extends JFrame implements ActionListener 
 {
+	
+    private JPanel contentPane;
+    private VideoCap capture = new VideoCap();
+    private JButton toggleCam;
+    private boolean activeCamera;
+    private static DisplayVideo frame;
+    private Image img;
 
-	private JFrame frame;
-	private JButton toggleCam;
-	private JPanel panel;
-	private VideoCap capture;
-	private Thread t;
-	private boolean activeCamera;
+    public static void main(String[] args) {
+        EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                try {
+                    frame = new DisplayVideo();
+                    frame.setVisible(true);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+    }
 
-	public DisplayVideo()
-	{				
-		System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
-		activeCamera = false;
-		panel = new JPanel();
-		frame = new JFrame("Video");
-		toggleCam = new JButton("Toggle Cam");
-		toggleCam.addActionListener(this);
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.setSize(800, 800);
-		frame.setVisible(true);
-		panel.add(Box.createHorizontalGlue());
-		panel.add(toggleCam);
-		panel.setLayout(new BoxLayout(panel, BoxLayout.LINE_AXIS));
-		frame.add(panel, BorderLayout.NORTH);
-		//capture = new VideoCapture();
-		//		capture.open(0);
-		t = new Thread();
-		t.start();
-	}
+    public DisplayVideo() throws IOException {
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setSize(800, 800);
+        toggleCam = new JButton("Toggle Camera");
+        toggleCam.addActionListener(this);
+        contentPane = new JPanel();
+        contentPane.setBorder(new EmptyBorder(0, 0, 0, 0));
+        setContentPane(contentPane);
+        contentPane.setLayout(null);
+        contentPane.add(Box.createHorizontalGlue());
+        contentPane.add(toggleCam);
+        contentPane.setLayout(new BoxLayout(contentPane, BoxLayout.LINE_AXIS));
+        Thread t = new MyThread();
+        t.start();
+        /*
+         * !!!!!!!!!!!!!!!!
+         * change this file location
+         * !!!!!!!!!!!!!!!!
+         */
+        img = ImageIO.read(new File("C:/Users/walkd/Documents/blackscreen.png"));
+    }
 
-	public static void main(String[] args)
-	{
-		new DisplayVideo();
-	}
+    public void paint(Graphics g){
+    	if(!activeCamera)
+    	{
+    		g.drawImage(img, 0, 0, this);
+    	} 
+    	else 
+    	{
+            g = contentPane.getGraphics();
+            g.drawImage(capture.getOneFrame(), 0, 0, this);
+    	}
+    }
 
+    class MyThread extends Thread{
+        public void run() {
+            for (;;){
+                repaint();
+                try 
+                { 
+                	Thread.sleep(33);
+                } 
+                catch (InterruptedException e) {
+                	e.printStackTrace();
+                }
+            }
+        }
+    }
+    
 	public void actionPerformed(ActionEvent e) {
 		if (e.getSource().equals(toggleCam))
 		{
 			activeCamera = !activeCamera;
-		}
-	}
-
-	public void paint(Graphics g)
-	{
-		g = panel.getGraphics();
-		//		g.drawImage(capture.getOneFrame(), 0, 0, this);
-	}
-
-	public void run()
-	{
-		while(activeCamera) {
-			//		repaint(g);
-			try {
-				Thread.sleep(33);
-			} catch (Exception e) {
-				e.printStackTrace();
-			}
 		}
 	}
 }
