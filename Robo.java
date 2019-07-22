91e8ed7f8c2c2ca39d3dabd5bb3148c9394a6027
sidharthdaga <50777618+sidharthdaga@users.noreply.github.com>
2019-07-22 11:33:59 -0700

    Add files via upload

--- Robo.java
+++ Robo.java
@@ -1,3 +1,4 @@
+package opencvtest2;
 import java.awt.AWTException;
 import java.awt.Graphics;
 import java.awt.Image;
@@ -8,14 +9,24 @@ import java.awt.event.InputEvent;
 import java.awt.event.KeyEvent;
 import java.awt.image.ImageObserver;
 import java.io.IOException;
+
+import javax.swing.JFrame;
+
 import java.awt.Robot; 
 
-public class Robo 
+public class Robo extends JFrame
 { 
-	@SuppressWarnings("null")
-	public static void main(String[] args) throws AWTException, InterruptedException
-	{
-		Robot robot = new Robot();
+	private Robot robot;
+	public Robo() {
+		try {
+			robot = new Robot();
+		} catch (AWTException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	public void Movemouse() throws InterruptedException {
+		System.out.println("helllooo");
 		robot.mouseMove(700,400); //move mouse
 		int button = InputEvent.getMaskForButton(1);
 		robot.mousePress(button);
@@ -29,18 +40,19 @@ public class Robo
 		robot.keyPress(KeyEvent.VK_L); 
 		Thread.sleep(500); 
 		robot.keyPress(KeyEvent.VK_O); 
-		Thread.sleep(500); Robot r = new Robot();
+		System.out.println("helllooo");
+	}
+	public void Scroll() throws InterruptedException {
+		Thread.sleep(500); 
 		for(int i = 0; i < 20; i++){ //scroll
-			r.mouseWheel(1);
+			robot.mouseWheel(1);
 			try{ Thread.sleep(50); }catch(InterruptedException e){}
 		}
-		Graphics g = null;
-		ImageObserver ImageObserver = null;
-		g.drawImage(Zoom(), 0, 0, 300, 300, ImageObserver); //zoom
-
 	}
-	public static Image Zoom() { try {
-		Robot robot = new Robot();
+	public void paint(Graphics g) {
+		g.drawImage(Zoom(), 0, 0, 300, 300, (ImageObserver)this);
+	}
+	public Image Zoom() { try {
 		Point p = MouseInfo.getPointerInfo().getLocation();
 		return robot.createScreenCapture(new Rectangle((int)p.getX(), (int) p.getY()));
 	} catch (Exception e) { 
