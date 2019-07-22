6740ee52892c642cceabe66eabd0f69215d50b4f
Sidharth Daga <siddy812@gmail.com>
2019-07-22 10:32:20 -0700

    jjkj

---
+++ Java Code/RobotUser.java
@@ -0,0 +1,9 @@
+package opencvtest2;
+
+public class RobotUser {
+	public static void main(String[]args) throws InterruptedException {
+		Robo robo = new Robo();
+		robo.Movemouse();
+//		robo.repaint();
+	}
+}
