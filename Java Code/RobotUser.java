da90a70f7c089561d71f6e0c6a5f3fc707284f05
Fillip <fillip.cannard@gmail.com>
2019-07-26 11:52:19 -0700

    Cleanup for final view

--- Java Code/RobotUser.java
+++
@@ -1,9 +0,0 @@
-package opencvtest2;
-
-public class RobotUser {
-	public static void main(String[]args) throws InterruptedException {
-		Robo robo = new Robo();
-		robo.Movemouse();
-//		robo.repaint();
-	}
-}
