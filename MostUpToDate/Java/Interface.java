acd22d5ba6f8ac506a325c49d27d68c36a9995df
David Zager <walkdownlane22@gmail.com>
2019-07-29 13:05:11 -0700

    Update Interface.java

--- MostUpToDate/Java/Interface.java
+++ MostUpToDate/Java/Interface.java
@@ -1,122 +1,121 @@
 package MyPackage;
 
-import java.awt.BorderLayout;
-import java.awt.Container;
-import java.awt.GridLayout;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
 
-import javax.swing.JButton;
-import javax.swing.JComboBox;
-import javax.swing.JFrame;
-import javax.swing.JLabel;
-import javax.swing.JPanel;
-
-public class Interface implements ActionListener{
+public class Interface extends JFrame implements ActionListener{
 
 	private JComboBox CommandList, CommandList1, CommandList2, CommandList3, CommandList4, CommandList5, CommandList6;
-	private JFrame frame;
 	private JLabel label, label1, label2, label3, label4, label5, label6;
 	private JPanel panel, panel1, panel2, panel3, panel4, panel5, panel6;
 	private JButton set, reset;
-	private int zoomin, zoomout, scrollup, scrolldown, swiper, swipel, sleep;
+	private int fist, openH, thumbU, thumbD, pointR, pointL, coverCam;
+	private int [] array;
+	private String[] commands;
 
 	public static void main(String[] args) {
 		new Interface();
 	}
 	public Interface() {
-		frame = new JFrame();
-		frame.setTitle("Hand Tracking");
-		frame.setSize(500,500);
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.setLayout(new GridLayout(7,2));
+		setTitle("Hand Tracking");
+		setSize(500,500);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setLayout(new GridLayout(7,2));
 		set = new JButton ("Set");
 		reset = new JButton ("Reset");
-		set.setSize(10, 10);
-		reset.setSize(10, 10);
-		set.setVisible(true);
-		reset.setVisible(true);
 		set.addActionListener(this);
 		reset.addActionListener(this);
-		frame.add(set);
-		frame.add(reset);
+		add(set);
+		add(reset);
+		
+		fist = 1;
+		openH = 2;
+		thumbU = 3;
+		thumbD = 4;
+		pointR = 5;
+		pointL = 6;
+		coverCam= 7;
+		
+		commands = new String[]{"Assign action for each gesture", "Zoom In", "Zoom Out", "Scroll Up", "Scroll Down", "Swipe Right", "Swipe Left", "Sleep"};
+		
 		panel = new JPanel();
 		panel.setSize(500, 70);
+		
 		panel1 = new JPanel();
 		panel1.setSize(500, 70);
+		
 		panel2 = new JPanel();
 		panel2.setSize(500, 70);
+		
 		panel3 = new JPanel();
 		panel3.setSize(500, 70);
+		
 		panel4 = new JPanel();
 		panel4.setSize(500, 70);
+		
 		panel5 = new JPanel();
 		panel5.setSize(500, 70);
+		
 		panel6 = new JPanel();
 		panel6.setSize(500, 70);
-		label = new JLabel("Zoom in");
+		
+		label = new JLabel("Closed Hand");
 		label.setSize(100, 100);
-		label.setVisible(true);
-		label1 = new JLabel("Zoom out");
+		
+		label1 = new JLabel("Open Hand");
 		label1.setSize(100, 100);
-		label1.setVisible(true);
-		label2 = new JLabel("Scroll up");
+
+		label2 = new JLabel("Thumbs Up");
 		label2.setSize(100, 100);
-		label2.setVisible(true);
-		label3 = new JLabel("Scroll down");
+
+		label3 = new JLabel("Thumbs Down");
 		label3.setSize(100, 100);
-		label3.setVisible(true);
-		label4 = new JLabel("Swipe right");
+
+		label4 = new JLabel("Point Right");
 		label4.setSize(100, 100);
-		label4.setVisible(true);
-		label5 = new JLabel("Swipe left");
+
+		label5 = new JLabel("Point Left");
 		label5.setSize(100, 100);
-		label5.setVisible(true);
-		label6 = new JLabel("Sleep");
-		label6.setSize(100, 100);
-		label6.setVisible(true);
 
-		//		String[]actions = {"Select ", "zoom in", "zoom out", "scroll up", "scroll down", "alt shift tab" ,"alt tab", "sleep"};
-		//		ActionList = new JComboBox(actions);
-		//		ActionList.setVisible(true);
-		//		ActionList.setSize(100,100);
-		//		frame.add(ActionList);
-
-		String[]commands = {"Assign action for each command", "fist", "open hand", "thumbs up", "thumbs down", "point right" ,"point left", "cover camera"};
+		label6 = new JLabel("Cover Camera");
+		label6.setSize(100, 100);		
+				
 		CommandList = new JComboBox(commands);
-		CommandList.setVisible(true);
+		CommandList.setSelectedIndex(1);
 		CommandList.setSize(100,100);
 		CommandList.addActionListener(this);
-		String[]commands1 = {"Assign action for each command", "fist", "open hand", "thumbs up", "thumbs down", "point right" ,"point left", "cover camera"};
-		CommandList1 = new JComboBox(commands1);
-		CommandList1.setVisible(true);
+	
+		CommandList1 = new JComboBox(commands);
+		CommandList1.setSelectedIndex(2);
 		CommandList1.setSize(100,100);
 		CommandList1.addActionListener(this);
-		String[]commands2 = {"Assign action for each command", "fist", "open hand", "thumbs up", "thumbs down", "point right" ,"point left", "cover camera"};
-		CommandList2 = new JComboBox(commands2);
-		CommandList2.setVisible(true);
+		
+		CommandList2 = new JComboBox(commands);
+		CommandList2.setSelectedIndex(3);
 		CommandList2.setSize(100,100);
 		CommandList2.addActionListener(this);
-		String[]commands3 = {"Assign action for each command", "fist", "open hand", "thumbs up", "thumbs down", "point right" ,"point left", "cover camera"};
-		CommandList3 = new JComboBox(commands3);
-		CommandList3.setVisible(true);
+		
+		CommandList3 = new JComboBox(commands);
+		CommandList3.setSelectedIndex(4);
 		CommandList3.setSize(100,100);
 		CommandList3.addActionListener(this);
-		String[]commands4 = {"Assign action for each command", "fist", "open hand", "thumbs up", "thumbs down", "point right" ,"point left", "cover camera"};
-		CommandList4 = new JComboBox(commands4);
-		CommandList4.setVisible(true);
+		
+		CommandList4 = new JComboBox(commands);
+		CommandList4.setSelectedIndex(5);
 		CommandList4.setSize(100,100);
 		CommandList4.addActionListener(this);
-		String[]commands5 = {"Assign action for each command", "fist", "open hand", "thumbs up", "thumbs down", "point right" ,"point left", "cover camera"};
-		CommandList5 = new JComboBox(commands5);
-		CommandList5.setVisible(true);
+		
+		CommandList5 = new JComboBox(commands);
+		CommandList5.setSelectedIndex(6);
 		CommandList5.setSize(100,100);
 		CommandList5.addActionListener(this);
-		String[]commands6 = {"Assign action for each command", "fist", "open hand", "thumbs up", "thumbs down", "point right" ,"point left", "cover camera"};
-		CommandList6 = new JComboBox(commands6);
-		CommandList6.setVisible(true);
+		
+		CommandList6 = new JComboBox(commands);
+		CommandList6.setSelectedIndex(7);
 		CommandList6.setSize(100,100);
 		CommandList6.addActionListener(this);
+		
 		panel.add(label);
 		panel.add(CommandList);
 		panel1.add(label1);
@@ -131,59 +130,59 @@ public class Interface implements ActionListener{
 		panel5.add(CommandList5);
 		panel6.add(label6);
 		panel6.add(CommandList6);	
-		frame.add(panel);
-		frame.add(panel1);
-		frame.add(panel2);
-		frame.add(panel3);
-		frame.add(panel4);
-		frame.add(panel5);
-		frame.add(panel6);
-		frame.setVisible(true);
+		add(panel);
+		add(panel1);
+		add(panel2);
+		add(panel3);
+		add(panel4);
+		add(panel5);
+		add(panel6);
+		setVisible(true);
 	}
-	@Override
+	
 	public void actionPerformed(ActionEvent e) {
 		Object source = e.getSource();
-		
 		if (source instanceof JComboBox) {
 			JComboBox cb = (JComboBox)e.getSource();
 			int selected = cb.getSelectedIndex();
 			if (e.getSource().equals(CommandList)) {
-				zoomin = selected;
+				fist = selected;
 			}
 			if (e.getSource().equals(CommandList1)) {
-				zoomout = selected;
+				openH = selected;
 			}
 			if (e.getSource().equals(CommandList2)) {
-				scrollup = selected;
+				thumbU = selected;
 			}
 			if (e.getSource().equals(CommandList3)) {
-				scrolldown = selected;
+				thumbD = selected;
 			}
 			if (e.getSource().equals(CommandList4)) {
-				swiper = selected;
+				pointR = selected;
 			}
 			if (e.getSource().equals(CommandList5)) {
-				swipel = selected;
+				pointL = selected;
 			}
 			if (e.getSource().equals(CommandList6)) {
-				sleep = selected;
+				coverCam = selected;
 			}
 		}
 		else if (source instanceof JButton) {
 			if (e.getSource().equals(set)) {
-				int [] array = {zoomin, zoomout, scrollup, scrolldown, swiper, swipel, sleep};
+				int [] array = {fist, openH, thumbU, thumbD, pointR, pointL, coverCam};
 				int count = 0;
-				for (int j = 0; j< array.length; j++) {
-					for (int i = j+1; i<array.length; i++) {
-						if (array[j] == array[i] || array[j]==0) {
+				for (int j = 0; j < array.length; j++) {
+					for (int i = j + 1; i < array.length; i++) {
+						if (array[j] == array[i] || array[j] == 0) {
 							count++;
 						}
 					}
 				}
-				if (count>0) {
-					set.setLabel("Error! Try again! Set");
+				if (count > 0) {
+					JOptionPane.showMessageDialog(this, "Please assign only one command for each gesture", "Error!", JOptionPane.ERROR_MESSAGE);
 				} else {
-					System.out.println("good work");
+					set.setLabel("Settings Updated!");
+					this.array = array;
 				}
 			}
 			if (e.getSource().equals(reset)) {
@@ -196,7 +195,8 @@ public class Interface implements ActionListener{
 				CommandList6.setSelectedIndex(0);
 			}
 		}
-
 	}
-
+	public int[] getArray() {
+		return array;
+	}
 }
