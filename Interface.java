b9bd87add81b059c9acf998b85b9f22d0e5f1678
sidharthdaga <50777618+sidharthdaga@users.noreply.github.com>
2019-07-29 10:30:58 -0700

    Add files via upload

--- Interface.java
+++ Interface.java
@@ -1,10 +1,12 @@
 package opencvtest2;
 
+import java.awt.BorderLayout;
 import java.awt.Container;
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+import javax.swing.JButton;
 import javax.swing.JComboBox;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
@@ -34,7 +36,16 @@ public class Interface implements ActionListener{
 	private JPanel panel4;
 	private JPanel panel5;
 	private JPanel panel6;
-	
+	private JButton set;
+	private JButton reset;
+	private int zoomin;
+	private int zoomout;
+	private int scrollup;
+	private int scrolldown;
+	private int swiper;
+	private int swipel;
+	private int sleep;
+
 	public static void main(String[] args) {
 		new Interface();
 	}
@@ -44,6 +55,16 @@ public class Interface implements ActionListener{
 		frame.setSize(500,500);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.setLayout(new GridLayout(7,2));
+		set = new JButton ("Set");
+		reset = new JButton ("Reset");
+		set.setSize(10, 10);
+		reset.setSize(10, 10);
+		set.setVisible(true);
+		reset.setVisible(true);
+		set.addActionListener(this);
+		reset.addActionListener(this);
+		frame.add(set);
+		frame.add(reset);
 		panel = new JPanel();
 		panel.setSize(500, 70);
 		panel.setVisible(true);
@@ -87,12 +108,12 @@ public class Interface implements ActionListener{
 		label6.setSize(100, 100);
 		label6.setVisible(true);
 
-//		String[]actions = {"Select ", "zoom in", "zoom out", "scroll up", "scroll down", "alt shift tab" ,"alt tab", "sleep"};
-//		ActionList = new JComboBox(actions);
-//		ActionList.setVisible(true);
-//		ActionList.setSize(100,100);
-//		frame.add(ActionList);
-		
+		//		String[]actions = {"Select ", "zoom in", "zoom out", "scroll up", "scroll down", "alt shift tab" ,"alt tab", "sleep"};
+		//		ActionList = new JComboBox(actions);
+		//		ActionList.setVisible(true);
+		//		ActionList.setSize(100,100);
+		//		frame.add(ActionList);
+
 		String[]commands = {"Assign action for each command", "fist", "open hand", "thumbs up", "thumbs down", "point right" ,"point left", "cover camera"};
 		CommandList = new JComboBox(commands);
 		CommandList.setVisible(true);
@@ -153,10 +174,61 @@ public class Interface implements ActionListener{
 	}
 	@Override
 	public void actionPerformed(ActionEvent e) {
-		JComboBox cb = (JComboBox)e.getSource();
-        int selected = cb.getSelectedIndex();
-        CommandList.removeItemAt(selected);
-      
+		Object source = e.getSource();
+		
+		if (source instanceof JComboBox) {
+			JComboBox cb = (JComboBox)e.getSource();
+			int selected = cb.getSelectedIndex();
+			if (e.getSource().equals(CommandList)) {
+				zoomin = selected;
+			}
+			if (e.getSource().equals(CommandList1)) {
+				zoomout = selected;
+			}
+			if (e.getSource().equals(CommandList2)) {
+				scrollup = selected;
+			}
+			if (e.getSource().equals(CommandList3)) {
+				scrolldown = selected;
+			}
+			if (e.getSource().equals(CommandList4)) {
+				swiper = selected;
+			}
+			if (e.getSource().equals(CommandList5)) {
+				swipel = selected;
+			}
+			if (e.getSource().equals(CommandList6)) {
+				sleep = selected;
+			}
+		}
+		else if (source instanceof JButton) {
+			if (e.getSource().equals(set)) {
+				int [] array = {zoomin, zoomout, scrollup, scrolldown, swiper, swipel, sleep};
+				int count = 0;
+				for (int j = 0; j< array.length; j++) {
+					for (int i = j+1; i<array.length; i++) {
+						if (array[j] == array[i] || array[j]==0) {
+							count++;
+						}
+					}
+				}
+				if (count>0) {
+					set.setLabel("Error! Try again! Set");	
+				} else {
+					System.out.println("good work");
+				}
+			}
+			if (e.getSource().equals(reset)) {
+				CommandList.setSelectedIndex(0);
+				CommandList1.setSelectedIndex(0);
+				CommandList2.setSelectedIndex(0);
+				CommandList3.setSelectedIndex(0);
+				CommandList4.setSelectedIndex(0);
+				CommandList5.setSelectedIndex(0);
+				CommandList6.setSelectedIndex(0);
+			}
+		}
+
 	}
 
 }
