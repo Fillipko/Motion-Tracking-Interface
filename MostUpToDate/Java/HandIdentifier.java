7b161dda07ba792f557a84f768b4e67673899117
David Zager <walkdownlane22@gmail.com>
2019-07-29 14:42:06 -0700

    Update HandIdentifier.java

--- MostUpToDate/Java/HandIdentifier.java
+++ MostUpToDate/Java/HandIdentifier.java
@@ -11,19 +11,30 @@ import org.opencv.objdetect.CascadeClassifier;
 public class HandIdentifier {
 	private int rectThickness = 3;
 	public int numOfFoundHands = 0;
+	private int count = 0;
+	private String[] xml;
 
 	public static void main(String[] args) {
 		System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
 	}
 
-	public Mat FindFace(Mat mat) {
-		String xmlFileName = "xml/lbpcascade_frontalface_improved.xml";
-		CascadeClassifier cc = new CascadeClassifier(xmlFileName);
-		MatOfRect handDetection = new MatOfRect();
-		cc.detectMultiScale(mat, handDetection);
-		numOfFoundHands = Integer.parseInt(String.format("%d", handDetection.toArray().length));
-		for (Rect rect : handDetection.toArray()) {
-			Imgproc.rectangle(mat, new Point(rect.x, rect.y), new Point(rect.x + rect.width, rect.y + rect.height), new Scalar(0, 0, 250), rectThickness);
+	public Mat FindFace(Mat mat) 
+	{
+		xml = new String[]{ "cascadeFist", "cascadeOpen"};
+		for(int i = 0; i < xml.length; i++)
+		{
+			CascadeClassifier cc = new CascadeClassifier("src/" + xml[i] + ".xml");
+			MatOfRect handDetection = new MatOfRect();
+			cc.detectMultiScale(mat, handDetection);
+			numOfFoundHands = Integer.parseInt(String.format("%d", handDetection.toArray().length));
+			for (Rect rect : handDetection.toArray()) {
+				Imgproc.rectangle(mat, new Point(rect.x, rect.y), new Point(rect.x + rect.width, rect.y + rect.height), new Scalar(0, 0, 250), rectThickness);
+				count++;
+			}
+			if(count != 0)
+			{
+				i = xml.length;
+			}
 		}
 		return mat;
 	}
